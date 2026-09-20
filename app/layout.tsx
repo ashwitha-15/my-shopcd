4c7a829b71aef871f06cceaea9e71d1b46053922
ashwitha-15 <ashureddyashu24@gmail.com>
2026-09-20 15:25:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,17 +13,14 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MY SHOP",
-  description: "MY SHOP - BEST Online Store",
+  title: "Style Hub",
+  description: "Style Hub - Hyderabad's Trending Fashion Store",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
     </html>
   );
-}
+  }
